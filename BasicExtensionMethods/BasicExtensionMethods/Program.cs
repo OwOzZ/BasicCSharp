5520997a62a966166ba72ce8ae074f97195ea582
OwOzZ <Young@youngdeMacBook-Pro.local>
2016-04-09 19:45:29 +0800

    extension methods

---
+++ BasicExtensionMethods/BasicExtensionMethods/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using BasicExtensionMethods;
+
+namespace BasicExtensionMethods
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+//			extension methods example
+			Directory directory = new Directory();
+			directory.Copy ("hehe");
+		}
+	}
+
+	public class Directory 
+	{
+		public static void Move()
+		{
+			// do something
+		}
+		public static void Delete()
+		{
+			// do something
+		}
+	}
+
+//	extention methods
+	public static class DirectoryExtension
+	{
+		public static void Copy(this Directory sourceDirectory, string secondParameter)
+		{
+			// do something
+			Console.WriteLine("extension method are called");
+		}
+	}
+}
